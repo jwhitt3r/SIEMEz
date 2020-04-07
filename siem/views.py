609b033e793abf729cc4ad90b98d68f41189a25d
eXit <exit@protonmail.com>
2020-04-07 16:38:15 +0100

    User Reg and Basic Setup Done
    User Registration via email, and home page layout in place.

--- siem/views.py
+++ siem/views.py
@@ -5,3 +5,5 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = "home.html"
 
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
--- siemez/urls.py
+++ siemez/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('accounts/', include('users.urls')), # new
     path('', include('siem.urls')),
 ]
